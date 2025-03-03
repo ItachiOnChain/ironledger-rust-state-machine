bdfb8104911b32b532d8b6f0c136c2349b156201
itachionchain <itachionchain@gmail.com>
2025-03-03 16:19:24 +0530

    Interacting with balance Pallet state

--- src/balances.rs
+++ src/balances.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap; // we are using this rust collection to store balances
+use std::collections::BTreeMap; // we are using this rust collection to store balances, in real blockchain balances are stored in a database
 
 //Here we want to store balance of each user
 pub struct Pallet{
@@ -7,9 +7,23 @@ pub struct Pallet{
 
 
 impl Pallet {
+	/// Create a new instance of the balances module.
 	pub fn new() -> Self {
-		Self {
-			balances: BTreeMap::new()
-		}
+		Self { balances: BTreeMap::new() }
+	}
+
+	/// Set the balance of an account `who` to some `amount`.
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		/* Insert `amount` into the BTreeMap under `who`. */
+        self.balances.insert(who.clone(), amount);
+		unimplemented!()
+	}
+
+	/// Get the balance of an account `who`.
+	/// If the account has no stored balance, we return zero.
+	pub fn balance(&self, who: &String) -> u128 {
+		/* Return the balance of `who`, returning zero if `None`. */
+        *self.balances.get(who).unwrap_or(0);
+		unimplemented!()
 	}
 }
