cf206efb7b34594d397bafbaf95deba6439e8f83
itachionchain <itachionchain@gmail.com>
2025-03-03 16:22:29 +0530

    balance tranfer function implemented + testing is also done

--- src/balances.rs
+++ src/balances.rs
@@ -14,16 +14,98 @@ impl Pallet {
 
 	/// Set the balance of an account `who` to some `amount`.
 	pub fn set_balance(&mut self, who: &String, amount: u128) {
-		/* Insert `amount` into the BTreeMap under `who`. */
         self.balances.insert(who.clone(), amount);
-		unimplemented!()
 	}
 
 	/// Get the balance of an account `who`.
 	/// If the account has no stored balance, we return zero.
 	pub fn balance(&self, who: &String) -> u128 {
-		/* Return the balance of `who`, returning zero if `None`. */
-        *self.balances.get(who).unwrap_or(0);
-		unimplemented!()
+        *self.balances.get(who).unwrap_or(&0)
 	}
+
+    /// Transfer `amount` from one account to another.
+	/// This function verifies that `from` has at least `amount` balance to transfer,
+	/// and that no mathematical overflows occur.
+	pub fn transfer(
+		&mut self,
+		caller: String,
+		to: String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
+
+        let new_caller_balance = caller_balance.checked_sub(amount)
+            .ok_or("Insufficient balance")?;
+
+        let new_to_balance = to_balance.checked_add(amount)
+            .ok_or("Overflow in transfer")?;
+
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // Import everything from the parent module
+
+    #[test]
+
+    fn init_balances() {
+	let mut balances = Pallet::new();
+
+	assert_eq!(balances.balance(&"alice".to_string()), 0);
+	balances.set_balance(&"alice".to_string(), 100);
+	assert_eq!(balances.balance(&"alice".to_string()), 100);
+	assert_eq!(balances.balance(&"bob".to_string()), 0);
+}
+
+    #[test]
+	fn transfer_balance() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&alice.to_string(), 100);
+        let _ = balances.transfer(alice.clone(), bob.clone(), 90);
+
+        assert_eq!(balances.balance(&alice), 10);
+        assert_eq!(balances.balance(&bob), 90);
+	}
+
+    #[test]
+    fn transfer_balance_insufficient() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&alice.to_string(), 100);
+        let result = balances.transfer(alice.clone(), bob.clone(), 110);
+
+        assert!(result.is_err());
+        assert_eq!(balances.balance(&alice), 100);
+        assert_eq!(balances.balance(&bob), 0);
+    }
+
+    #[test]
+    fn transfer_balance_overflow() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&alice.to_string(), 100);
+        balances.set_balance(&bob.to_string(), u128::MAX);
+
+        let result = balances.transfer(alice.clone(), bob.clone(), 1);
+
+        assert_eq!(result, Err("Overflow in transfer"));
+        assert_eq!(balances.balance(&alice), 100);
+        assert_eq!(balances.balance(&bob), u128::MAX);
+    }
+
 }
