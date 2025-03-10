6f1440940911c7b5e3496170dec1836873104d9a
itachionchain <itachionchain@gmail.com>
2025-03-11 01:12:49 +0530

    adding dispatch function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use crate::support::Dispatch;
 
 mod balances;
-mod system;
 mod support;
+mod system;
 
-mod types{
+mod types {
     use crate::support;
 
     pub type AccountId = String;
@@ -16,16 +16,21 @@ mod types{
     pub type Block = support::Block<Header, Extrinsic>;
 }
 
-pub enum RuntimeCall {}
+pub enum RuntimeCall {
+    BalancesTransfer {
+        to: types::AccountId,
+        amount: types::Balance,
+    },
+}
 
 impl system::Config for Runtime {
     type AccountId = types::AccountId;
-    type BlockNumber = types::BlockNumber; 
+    type BlockNumber = types::BlockNumber;
     type Nonce = types::Nonce;
 }
 
 impl balances::Config for Runtime {
-    type Balance = types::Balance;    
+    type Balance = types::Balance;
 }
 
 #[derive(Debug)]
@@ -38,53 +43,59 @@ pub struct Runtime {
 }
 
 impl Runtime {
-	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
-	fn new() -> Self {
-		Self { 
+    // Create a new instance of the main Runtime, by creating a new instance of each pallet.
+    fn new() -> Self {
+        Self {
             system: system::Pallet::new(),
             balances: balances::Pallet::new(),
-         }
-	}
+        }
+    }
 
     fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
-		self.system.inc_block_number();
+        self.system.inc_block_number();
 
-        if (self.system.block_number()!= block.header.block_number) {
+        if (self.system.block_number() != block.header.block_number) {
             return Err("Block number mismatch");
         }
 
-        for (i ,support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
             // Increment the nonce for the caller
             self.system.inc_nonce(&caller);
 
             // Dispatch the call
             let _ = self.dispatch(caller, call).map_err(|e| {
-                eprintln!("Extrensic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
-                block.header.block_number, i, e);
+                eprintln!(
+                    "Extrensic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                );
             });
         }
-		Ok(())
-	}
+        Ok(())
+    }
 }
 
 //also ADD THIS CODE TO YOUR main.rs file:
 impl crate::support::Dispatch for Runtime {
-	type Caller = <Runtime as system::Config>::AccountId;
-	type Call = RuntimeCall;
-	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
-	//
-	// Dispatch allows us to identify which underlying module call we want to execute.
-	// Note that we extract the `caller` from the extrinsic, and use that information
-	// to determine who we are executing the call on behalf of.
-	fn dispatch(
-		&mut self,
-		caller: Self::Caller,
-		runtime_call: Self::Call,
-	) -> support::DispatchResult {
-		unimplemented!();
-	}
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
+    //
+    // Dispatch allows us to identify which underlying module call we want to execute.
+    // Note that we extract the `caller` from the extrinsic, and use that information
+    // to determine who we are executing the call on behalf of.
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        match runtime_call {
+            RuntimeCall::BalancesTransfer { to, amount } => {
+                self.balances.transfer(caller, to, amount)?;
+            }
+        }
+        Ok(())
+    }
 }
-
 
 fn main() {
     let mut runtime = Runtime::new();
@@ -104,14 +115,18 @@ fn main() {
     runtime.system.inc_nonce(&rajkumar);
 
     //First transaction
-    let _ = runtime.balances.transfer(rajkumar.clone(), dayitva.clone(), 50)
-    .map_err(|e| println!("Error: {:?}", e));
+    let _ = runtime
+        .balances
+        .transfer(rajkumar.clone(), dayitva.clone(), 50)
+        .map_err(|e| println!("Error: {:?}", e));
 
-    runtime.system.inc_nonce( &rajkumar);
+    runtime.system.inc_nonce(&rajkumar);
 
     // Second transaction
-    let _ = runtime.balances.transfer(dayitva.clone(), aditya.clone(), 30)
-    .map_err(|e| println!("Error: {:?}", e));
+    let _ = runtime
+        .balances
+        .transfer(dayitva.clone(), aditya.clone(), 30)
+        .map_err(|e| println!("Error: {:?}", e));
 
     println!("{:?}", runtime);
 }
